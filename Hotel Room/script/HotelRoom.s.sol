20a3869e186f0111468db5a6a821f8c47bc2c8dc
Wesley Luna <77464680+well0x01@users.noreply.github.com>
2024-06-11 18:08:55 -0300

    Create HotelRoom.s.sol

---
+++ Hotel Room/script/HotelRoom.s.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HotelRoom} from "../src/HotelRoom.sol";
+
+contract HotelRoomScript is Script {
+  function run() external {
+    // Inicializar o contrato
+    vm.startBroadcast();
+    HotelRoom hotelRoom = new HotelRoom();
+    vm.stopBroadcast();
+
+    address client1 = vm.addr(1);
+
+    vm.deal(client1, 1 ether);
+
+    vm.startBroadcast(client1);
+    hotelRoom.book{value: 1 ether}();
+    vm.stopBroadcast();
+
+    console.log("Status do quarto apos reservar: ", uint(hotelRoom.roomStatusCurrent()));
+    console.log("Parabens, voce reservou um quarto!", hotelRoom.owner());
+
+  }
+}
